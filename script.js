be7c2fbf2ae238c1c51bd78a2bc298bad267e9c7
Robert Schmahl <rschm007@gmail.com>
2020-10-07 18:21:37 -0700

    Added jquery logic for Question One and Q1 multiple choice inputs

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-    // Gets Link for Theme Song
-    var audioElement = document.createElement("audio");
-    audioElement.setAttribute("src", "assets/Pokemon-Theme-Song.mp3");
-    
-    // Music Button
-    $(".theme-button").on("click", function() {
-        audioElement.play();
-      });
-      $(".pause-button").on("click", function() {
-        audioElement.pause();
-      });
-  
-    // When button is pressed, change to quiz questions
-    $(".quizBtn").on("click", function() {
+// Define vars for question arguments
+var questionOne = "<div class='d-flex justify-content-center'><h3>Question 1: Commonly used data types DO NOT include: </h3> </div>"
+var questionTwo = "<div class='d-flex justify-content-center'>><h3>Question 1: The condition in an if/else statement is enclosed within: </h3> </div>"
+var questionThree = "<div class='d-flex justify-content-center'>><h3>Question 1: Arrays in JavaScript can be used to store: </h3> </div>"
+var questionFour = "<div class='d-flex justify-content-center'>><h3>Question 1 String values must be enclosed within ___ when being assigned to variables :</h3> </div>"
+var questionFive = "<div class='d-flex justify-content-center'>><h3>Question 1: String values must be enclosed within ___ when being assigned to variables </h3> </div>"
 
+// define vars for multiple choice questions
+var qstOneInput = [ 
+    "<button type='button' class='btn-secondary incorrect'>1. strings</button>",
+    "<button type='button' class='btn-secondary incorrect'>2. booleans</button>",
+    "<button type='button' class='btn-secondary correct'>3. alerts</button>",
+    "<button type='button' class='btn-secondary incorrect'>4. numbers</button>",
+]
+
+// define a variable for a blank unordered list to append question choices into
+var list = $("#qst-Input");
+
+// define a variable for the list parent
+var parent = list.parent();
+
+// Gets Link for Theme Song
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "assets/Pokemon-Theme-Song.mp3");
+
+// Music Button
+$(".theme-button").on("click", function() {
+    audioElement.play();
     });
+    $(".pause-button").on("click", function() {
+    audioElement.pause();
+    });
+
+// When button is pressed, change to quiz questions
+$(".quizBtn").on("click", function() {
+    $(this).remove();
+    // append questionOne header variable
+    $(".container").prepend(questionOne);
+    // create function to append qstOneInput into the qst-Input unordered list DOM...
+    // detach the list from the DOM so it doesn't refresh the DOM for every list item added and empty the list to remove existing values
+    list.detach().empty().each(function(i) {
+        // define a loop
+        for (var i = 0; i < qstOneInput.length; i++) {
+            // append a list with each item in the array
+            $(this).append("<ul>" + qstOneInput[i] + "</ul>");
+            if (i == qstOneInput.length - 1) {
+                // at end of loop append the full list back into DOM
+                $(this).appendTo(parent);
+            }
+        }
+    })
+});
